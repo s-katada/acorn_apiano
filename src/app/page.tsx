8a7769d9813998a003cf8ecdf0bd0a4205b128aa
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2024-01-29 21:19:27 +0900

    ざっくり完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from "next/image";
+'use client';
+import { Layout, Flex, Typography } from 'antd';
+import Image from 'next/image';
 
+const headerStyle: CSSProperties = {
+  background: 'transparent',
+  height: '10vh',
+  zIndex: 1,
+};
+
+const contentStyle: CSSProperties = {
+  background: 'transparent',
+  fontFamily: 'fangsong',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  zIndex: 1
+};
+
+const footerStyle: CSSProperties = {
+  background: 'transparent',
+  height: '10vh',
+  zIndex: 1,
+};
+
+const { Header, Footer, Sider, Content } = Layout;
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Layout>
+      <Image
+        src='/piano.jpeg'
+        alt='ピアノ'
+        layout='fill'
+        object='cover'
+        style={{ filter: 'brightness(60%)', width: '100%', height: '100%' }}
+        zIndex={0}
+      />
+      <Header style={headerStyle}>
+        <Flex horizon>
+          <Image
+            src='/donguri.png'
+            alt='どんぐり'
+            height={80}
+            width={80}
+          />
+          <Typography.Title
+            level={3}
+            style={{ width: '100%',
+                     color: 'white',
+                     fontFamily: 'impact',
+                     zIndex: 1,
+                     textAlign: 'center',
+                     margin: 'auto'
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            どんぐりピアノ教室
+          </Typography.Title>
+        </Flex>
+      </Header>
+      <Content style={contentStyle}>
+        <div style={{margin: '30vh 0 50vh 0', color: 'white'}}>
+          <Typography.Title level={3} style={{color: 'white'}}>ABOUT</Typography.Title>
+          <p>岐阜県中津川市福岡にある小さなピアノ教室です。
+            <br />
+            どんぐりのように小さなお子様から大人の方までどんな方でも楽しくピアノを弾けるようになることを目標にしています。
+            <br />
+            お気軽にお問い合わせください。
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div style={{margin: '50vh 0 30vh 0'}}>
+          <Typography.Title level={3}>講師紹介</Typography.Title>
+          <Image
+            src='/teacher.png'
+            alt='先生'
+            height={300}
+            width={300}
+            style={{borderRadius: '50%'}}
+          />
+          <p>中津川市出身。中津川市立福岡小学校、中津川市立福岡中学校、岐阜県立中津高等学校、名古屋芸術大学音楽学部ピアノ専攻卒業。
+            <br />
+            2008年よりどんぐりピアノ教室を開講。
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </Content>
+      <Footer style={footerStyle}>
+        <div>
+          <Typography.Title level={3} style={{color: 'white'}}>CONTACT</Typography.Title>
+          <p>お問い合わせはこちらから</p>
+        </div>
+      </Footer>
+    </Layout>
   );
 }
